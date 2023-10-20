5cd0fd37d580c76acf4601cffe291b43bc415fe8
Martin <mlienhard@indeed.com>
2023-10-20 16:09:54 -0700

    first commit

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,11 +7,149 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
     }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+struct ContentView: View {
+    @State private var animationAmountCG: CGFloat = 1
+    @State private var animationAmount = 0.0
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        ///*
+        // Building custom transitions using ViewModifier
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .transition(.pivot)
+            }
+        }
+        //*/
+        
+        /*
+        // Showing and hiding views with transitions
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
+        */
+        
+        /*
+        // Animating gestures
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 20))
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged {
+                    self.dragAmount = $0.translation
+                }
+                .onEnded { _ in
+                    withAnimation(.spring()) {
+                        self.dragAmount = .zero
+                    }
+                    self.enabled.toggle()
+                }
+        )
+        */
+        
+        /*
+        // Controlling the animation stack
+        Button("Tap Me") {
+            self.enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(.default)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        */
+        
+        /*
+        // Creating explicit animations
+        Button("Tap me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }.padding(50)
+        .background(Color.red)
+        .foregroundColor(Color.white)
+        .clipShape(Circle())
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 0, y: 1, z: 0)
+        )
+        */
+        
+        /*
+        // Customizing animations in SwiftUI
+        Button("Tap Me") {
+            // self.animationAmountCG += 1
+        }
+        .padding(40)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmountCG)
+                .opacity(Double(2 - animationAmountCG))
+                .animation(
+                    Animation.easeOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+        .onAppear {
+            self.animationAmountCG = 2
+        }
+        */
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
